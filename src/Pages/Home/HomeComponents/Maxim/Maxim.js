ebeef5798c3e2be4001326456d1a95b34cb5de85
drifter0910 <canhlam09102000@gmail.com>
2022-07-13 22:42:39 +0700

    slider

--- src/Pages/Home/HomeComponents/Maxim/Maxim.js
+++ src/Pages/Home/HomeComponents/Maxim/Maxim.js
@@ -1,54 +1,54 @@
 import React from "react";
 import "./Maxim.scss";
-import { EffectFade, Navigation, Pagination, Autoplay } from "swiper";
 import "swiper/css/effect-fade";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
-import { Swiper, SwiperSlide } from "swiper/react";
-
+import { Carousel } from "antd";
 const Maxim = () => {
+  const contentStyle = {
+    textAlign: "center",
+  };
   return (
     <div className="maxim">
       <div className="maxim-label">”</div>
       <div className="maxim-swiper-wrapper">
-        <Swiper
-          pagination={true}
-          autoplay={{ delay: 3000 }}
-          modules={[Pagination, Autoplay]}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <div className="maxim-wrapper">
-              <p>
-                Dolor id nibh ultricies vehicula ut id elit. Aenean eu leo quam.
-                Pellentesque ornare sem lacinia quam venenatis vestibulum.
-                Maecenas sed diam eget risus varius blandit sit amet non magna.
-              </p>
-              <h2>LISA HEART</h2>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="maxim-wrapper">
-              <p>
-                Dolor id nibh ultricies vehicula ut id elit. Aenean eu leo quam.
-                Pellentesque ornare sem lacinia quam venenatis vestibulum.
-                Maecenas sed diam eget risus varius blandit sit amet non magna.
-              </p>
-              <h2>JOHN SNOW</h2>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="maxim-wrapper">
-              <p>
-                Dolor id nibh ultricies vehicula ut id elit. Aenean eu leo quam.
-                Pellentesque ornare sem lacinia quam venenatis vestibulum.
-                Maecenas sed diam eget risus varius blandit sit amet non magna.
-              </p>
-              <h2>TRUMP</h2>
-            </div>
-          </SwiperSlide>
-        </Swiper>
+        <Carousel autoplay autoplaySpeed={3000}>
+          <div>
+            <h3 style={contentStyle}>
+              <div className="maxim-wrapper">
+                <p>
+                  Dolor id nibh ultricies vehicula ut id elit. Aenean eu leo
+                  quam.
+                </p>
+                <h2>LISA HEART</h2>
+              </div>
+            </h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>
+              <div className="maxim-wrapper">
+                <p>
+                  Dolor id nibh ultricies vehicula ut id elit. Aenean eu leo
+                  quam.
+                </p>
+                <h2>JOHN SNOW</h2>
+              </div>
+            </h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>
+              {" "}
+              <div className="maxim-wrapper">
+                <p>
+                  Dolor id nibh ultricies vehicula ut id elit. Aenean eu leo
+                  quam.
+                </p>
+                <h2>TRUMP</h2>
+              </div>
+            </h3>
+          </div>
+        </Carousel>
       </div>
     </div>
   );
